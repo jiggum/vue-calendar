f5b17b579153bf804f1698eb9f05f44d1aa970f7
min <0s.dongmin@gmail.com>
2020-08-09 18:44:51 +0900

    add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="Button" @click="onClick">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Button',
+  props: ['onClick'],
+})
+</script>
+
+<style scoped lang="scss">
+.Button {
+  width: 100px;
+  height: 44px;
+  font-size : 15px;
+  background-color: #eeeeee;
+  color: #007AFF;
+  border-radius: 8px;
+  cursor: pointer;
+  line-height: 44px;
+  text-align: center;
+}
+</style>
