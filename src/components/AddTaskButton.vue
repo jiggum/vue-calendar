3ea504c8e688854780e4ca5d2b8d7cdd869cf159
min <0s.dongmin@gmail.com>
2020-08-09 18:45:01 +0900

    add AddTaskButton component

---
+++ src/components/AddTaskButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="AddTaskButton" @click="onClick">
+    <svg width="20" height="20" viewBox="0 0 20 20">
+      <path d="M2 10H18" stroke="#000000" stroke-width="2"/>
+      <path d="M10 2L10 18" stroke="#000000" stroke-width="2"/>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AddTaskButton',
+  props: ['onClick'],
+})
+</script>
+
+<style scoped lang="scss">
+.AddTaskButton {
+  width: 100%;
+  height: 68px;
+  border-radius: 12px;
+  border: 2px solid #000000;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  margin: 4px 0;
+}
+</style>
